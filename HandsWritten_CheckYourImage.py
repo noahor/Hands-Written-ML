a142d48ed801d60a6c9755fa1dfbca75003d83ca
Oron <oronemail@gmail.com>
2021-04-29 15:16:33 +0300

    fix_modle

--- HandsWritten_CheckYourImage.py
+++ HandsWritten_CheckYourImage.py
@@ -15,13 +15,13 @@ import imageio
 
 
 # 1. Choose your Picture location 
-lina_color= io.imread('C:\\Users\\oron.noah\\OneDrive - Zerto\\DeepLearining Workshop\\HandsWritten_Ex\\7.png')
+lina_color= io.imread('4.png')
 lina_color = resize(lina_color,(28,28))
 image_data = color.rgb2gray(lina_color)
 
 
 # 2. Load Your Model 
-model = load_model("C:\\Users\\oron.noah\\Documents\\MY_ML\\mnistCNN_1.h5")
+model = load_model("HandWritten.h5")
 im2arr = image_data.reshape(1,28,28,1)
 im2arr = image_data.reshape(1, 28*28)
 # Predicting the Test set results
